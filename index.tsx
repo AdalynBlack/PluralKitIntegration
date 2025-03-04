dcb66b942119d1b3aaead3fe28941ee8dce23c55
Adalyn Black <tomblackaz@gmail.com>
2025-03-03 19:59:52 -0700

    fix: updated renderUsername patch to match properly

--- index.tsx
+++ index.tsx
@@ -277,7 +277,7 @@ export default definePlugin({
         {
             find: '?"@":""',
             replacement: {
-                match: /(?<=onContextMenu:\i,children:).*?\)}/,
+                match: /(?<=onContextMenu:\i,children:).*?}/,
                 replace: "$self.renderUsername(arguments[0])}"
             }
         },
